90953785dfb52c4c9ed4bcd3e8816c18e8b2f9ed
LUCAS RUSSEFF <lucas_russeff@estudante.sesisenai.org.br>
2025-06-13 12:16:47 -0300

    upload code final

---
+++ codigo/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Contato from './Contato'
+import Localizacao from './Mapa'
+import DireitosAutorais from './DireitosAutorais'
+
+function Footer() {
+  return (
+    <div>
+      <Contato/>
+      <Localizacao/>
+      <DireitosAutorais/>
+    </div>
+
+    
+  )
+}
+
+export default Footer
